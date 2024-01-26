d9386e8cc8815bb0d9e0ad3ef750ee1f025ea09b
D.J. Morvay <djmorvay@me.com>
2024-01-26 21:30:14 +0000

    LoRa GS code.

--- Python/LoRa_GS.py
+++ Python/LoRa_GS.py
@@ -8,6 +8,8 @@ def on_recv(payload):
     print("From:", payload.header_from)
     print("Received image")
     print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+    global received_success 
+    received_success = True
 
 # Use chip select 0. GPIO pin 17 will be used for interrupts
 # The address of this device will be set to 2
@@ -15,10 +17,27 @@ lora = LoRa(0, 19, 10, modem_config=ModemConfig.Bw125Cr45Sf128, acks=False)
 lora.on_recv = on_recv
 
 signal.signal(signal.SIGINT, lora.close)
-lora.set_mode_rx()
+
+received_success = False
 
 print('I am running...')
 
 while True:
-    time.sleep(0.1)
+    received_success = False
+    lora.set_mode_rx()
+
+    while received_success == False:
+        time.sleep(1)
+
+    lora.set_mode_tx()
+    time.sleep(1)
+    
+    ack = "GS is sending acknowledgement!"
+    status = lora.send(ack, 2)
+
+    if status is True:
+        print(ack)
+    else:
+        print("No acknowledgment from recipient")
+
 lora.close()
